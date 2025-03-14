f556929350b92f0305d859cf288eea7c15842aca
bilalkhan <bilalkhan751150@gmail.com>
2025-03-14 11:16:39 +0500

    blog posts page added where all the blogs will be shown

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -1,11 +1,43 @@
-import React from 'react'
+"use client"
+import React, { useState, useEffect } from 'react';
 
-const blogs = () => {
+const Blogs = () => {
+  const [posts, setPosts] = useState([]);
+
+  const fetchPosts = async () => {
+    let res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    let data = await res.json();
+    setPosts(data);
+    console.log(data);
+    console.log('fetching data from server');
+  };
+
+  useEffect(() => {
+    console.log("Fetching posts...");
+    fetchPosts();
+    console.log("Posts fetched!");
+  }, []);
+
   return (
-    <div>
-      All blogs are here
+    <div className="p-5 flex bg-gray-100 gap-3 items-center justify-center flex-wrap">
+      {posts.map((post) => (
+        <div key={post.id} className="card w-[348px] max-w-md h-[456px] bg-white shadow-lg rounded-lg overflow-hidden">
+          <div className="img w-full h-48 bg-gray-400">
+            <img src={post.src ? post.src : "empty.jpg"} alt="Post Image" className="w-full h-full object-cover" />
+          </div>
+          <div className="content p-4 text-black flex items-center flex-col">
+            <div className='h-[190px] overflow-ellipsis'>
+              <h1 className="text-2xl font-bold mb-2">{post.title}</h1>
+              <p className="text-gray-700 mb-4">{post.body}</p>
+            </div>
+            <div className="btn">
+              <button className="bg-green-500 text-white px-4 py-2 rounded-2xl hover:bg-green-600 transition-all">Read More</button>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default blogs
+export default Blogs;
